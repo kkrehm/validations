b675433b23257578f0afbaf2092237e45e2eaca6
kkrehm <rehm.katalin@gmail.com>
2025-03-11 11:52:17 +0000

    Update validations.py
    Adding a "test" comment

--- validations.py
+++ validations.py
@@ -3,6 +3,7 @@
 
 def validate_user(username, minlen):
   """Checks if the received username matches the required conditions."""
+  #Test
   if type(username) != str:
     raise TypeError("username must be a string")
   if minlen < 1:
